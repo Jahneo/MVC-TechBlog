5e128a71d02cdc4d311a99a0df9c0d75cafd39bf
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-10-20 01:50:38 -0400

    psot delete edit

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-//const User = require('./User');
-const userRoutes = require('./user-routes.js');
 
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 router.use('/users', userRoutes);
-
+router.use('/posts', postRoutes);
 module.exports = router;
