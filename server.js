c8ed01ca5919991f3d6ef5218cd134b617b78f01
Jahneo <neil4hall@gmail.com>
2021-11-21 02:13:32 -0500

    edit files and add functionality

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-const exphbs = require('express-handlebars');
+const PORT = process.env.PORT || 3010;
+const sequelize = require('./config/connection');
+
 const hbs = exphbs.create({});
+
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -16,9 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'develop')));
 // turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening on'));
 });
